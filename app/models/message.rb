b45b29c3f50c1e11f800739fe21cf021f23fd907
tomoyasai <sugi97421yama84619@gmail.com>
2021-02-26 15:30:24 +0900

    入力項目のバリデーションを追加

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,13 +1,15 @@
 class Message < ApplicationRecord
     belongs_to :user
     has_many :comments
-
-    validates :content, presence: true, unless: :image?
-
     mount_uploader :image, ImageUploader
+    with_options presence: true do
+      validates :title
+      validates :content
+      validates :category_id
+      validates :image
+    end
 
     extend ActiveHash::Associations::ActiveRecordExtensions
     belongs_to_active_hash :category
-    validates :category, presence: true
 
 end
