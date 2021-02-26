f49030d621ef5a2020ce1ffe6faf34369d69cbf9
tomoyasai <sugi97421yama84619@gmail.com>
2021-02-26 15:29:40 +0900

    データが保存できるように記述

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,5 +3,22 @@ class MessagesController < ApplicationController
   end
 
   def new
+    @message = Message.new
   end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.valid?
+      @message.save
+      redirect_to root_path
+    else
+      render "new"
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:title, :content, :image,:category_id).merge(user_id: current_user.id)
+  end
+
 end
