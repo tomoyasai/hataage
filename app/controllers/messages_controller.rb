641eb7e6845c6163fc6747f05fb0d354e35a9782
tomoyasai <sugi97421yama84619@gmail.com>
2021-02-26 15:50:04 +0900

    投稿時のフラッシュメッセージを作成

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,8 +10,9 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
     if @message.valid?
       @message.save
-      redirect_to root_path
+      redirect_to root_path, notice: 'メッセージが送信されました'
     else
+      flash.now[:alert] = '必要項目を入力してください。'
       render "new"
     end
   end
