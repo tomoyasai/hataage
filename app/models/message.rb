62cacd77f735463ff58eeea0966be2f803acdca3
tomoyasai <sugi97421yama84619@gmail.com>
2021-02-26 14:44:25 +0900

    アソシエーションを定義

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,10 @@ class Message < ApplicationRecord
     has_many :comments
 
     validates :content, presence: true, unless: :image?
-    
+
     mount_uploader :image, ImageUploader
+
+    extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to_active_hash :category
+
 end
