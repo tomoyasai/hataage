0b2498aaba85c00e3d0e8531a1383e40f15a780a
tomoyasai <sugi97421yama84619@gmail.com>
2021-02-27 14:50:01 +0900

    ルーティングを追記

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "messages#index"
 
-  resources :messages, only: [:index, :new, :create]  
+  resources :messages, only: [:index, :new, :create, :show]
   resources :users, only: [:edit, :update]
 end
