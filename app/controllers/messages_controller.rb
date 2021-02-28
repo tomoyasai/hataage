dca7f8265a41e7b2b383326e801ef653ac194bd6
tomoyasai <sugi97421yama84619@gmail.com>
2021-02-28 15:42:11 +0900

    destroyアクションを追加

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -25,6 +25,16 @@ class MessagesController < ApplicationController
     end
   end
 
+  def destroy
+    message = Message.find(params[:id]) 
+    if current_user.id == message.user.id
+      message.destroy
+      redirect_to root_path
+    else 
+      redirect_to message_path(message.id)
+    end
+  end
+
   def create
     @message = Message.new(message_params)
     if @message.valid?
