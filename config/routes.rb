2326d3efc817b1abd8797779248e75b69b0169b3
tomoyasai <sugi97421yama84619@gmail.com>
2021-03-02 15:26:38 +0900

    ルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "messages#index"
 
-  resources :messages, only: [:index, :new, :create, :show] do 
+  resources :messages, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
     collection do
       get 'search'
     end
 
     post 'add' => 'likes#create'
     delete '/add' => 'likes#destroy'
-    
   end
-
-  resources :messages, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 
   resources :users, only: [:edit, :update]
 end
