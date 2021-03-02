d4b2ecc3b3b153a00d3981f19457a73e3948eae4
tomoyasai <sugi97421yama84619@gmail.com>
2021-03-02 11:57:05 +0900

    各アクションを定義

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,21 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_like
+
+  def create
+      user = current_user
+      message = Message.find(params[:message_id])
+      like = Like.create(user_id: user.id, message_id: message.id)
+  end
+  def destroy
+      user = current_user
+      message = Message.find(params[:message_id])
+      like = Like.find_by(user_id: user.id, message_id: message.id)
+      like.delete
+  end
+
+  private
+  def set_like
+      @message =Message.find(params[:message_id])
+  end
 end
