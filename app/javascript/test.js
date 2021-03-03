5ae4079cc0ef54bc1bc9255b810cd02d6a97b3a9
tomoyasai <sugi97421yama84619@gmail.com>
2021-03-03 19:18:29 +0900

    jsファイルを修正

--- app/javascript/test.js
+++ app/javascript/test.js
@@ -1,38 +1,26 @@
-const { handleMethod } = require("@rails/ujs");
+// const { handleMethod } = require("@rails/ujs");
 
 $(function(){
-  $('.item-button-container').on('click','.like', function(e){
+  $('.item-button-container').on('click','.unlike', function(e){
+    let str = $(".item-button-container span").text();
     // debugger
-    var str = $(".item-button-container span").text();
-    if (method = post) {
-      str = (+str + 1);
-      });
-    } else {
-      str = (-str + 1);
-    }
+  
     
+      str = +str + 1;
+  
     $(".item-button-container span").text(str);
-
     });
-});
 
-// $("button").click(function(){
-// 	var str = $("p").text();
-// 	alert(str);
-// })
+    $('.item-button-container').on('click','.liked', function(e){
+      let str = $(".item-button-container span").text();
+      // debugger
+    
+      
+        str = +str -1;
+    
+      $(".item-button-container span").text(str);
+      });
+ });
 
-// $(".item-button-container span").text(str);
-//   str = (str + 1)
 
-// .done(function(users) {
-//   $("#UserSearchResult").empty();
-//   if (users.length !== 0) {
-//     users.forEach(function(user) {
-//       addUser(user);
-//     });
-//   } else if (input.length == 0) {
-//     return false;
-//   } else {
-//     addNoUser();
-//   }
-// })
+//＋strの＋は文字を数値にする意味がある
