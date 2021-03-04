94c34fd0e4f32b08bb0a409cd8aae7e3b01b3b09
tomoyasai <sugi97421yama84619@gmail.com>
2021-03-04 19:25:06 +0900

    createアクションを定義し、ストロングパラメーターも利用

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,10 @@
 class CommentsController < ApplicationController
+  def create
+    Comment.create(comment_params)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, message_id: params[:message_id])
+  end
 end
