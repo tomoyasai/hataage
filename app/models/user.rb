a12335278a5b0ce2f94a780652ada49b05b518d2
tomoyasai <sugi97421yama84619@gmail.com>
2021-03-04 19:04:45 +0900

    アソシエーションを追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :messages
-  has_many :comments
+  has_many :comments 
   has_many :likes, dependent: :destroy
   has_many :like_messages, through: :likes, source: :message
   
