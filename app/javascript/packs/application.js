28ff0e5a5750bc4e554d4a1d53b268f397cc0d56
tomoyasai <sugi97421yama84619@gmail.com>
2021-03-05 18:50:50 +0900

    turbolinksをコメントアウト

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 require('jquery')
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("comment.js")
