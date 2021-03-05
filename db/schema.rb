c56c08cbda19933f5e4d7e42d43ccbe8ad1868a6
tomoyasai <sugi97421yama84619@gmail.com>
2021-03-05 11:27:43 +0900

    マイグレーションファイルを修正

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2021_03_04_095358) do
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "message_id", null: false
-    t.text "comment", null: false
+    t.text "text", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["message_id"], name: "index_comments_on_message_id"
