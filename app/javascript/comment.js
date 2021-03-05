1ffa94ba7f40116ad7d7c19c8ee271a029f614c7
tomoyasai <sugi97421yama84619@gmail.com>
2021-03-05 16:41:23 +0900

    jsファイルを追記

--- app/javascript/comment.js
+++ app/javascript/comment.js
@@ -1,3 +1,32 @@
 $(function(){
-  console.log(1234)
+  function buildHTML(comment){
+    let html = `<div class="comment-box">
+                  <div class="comment-name">
+                  ${comment.user_name}
+                  </div>
+                  <div class="comment-text">
+                  ${comment.text}
+                  </div>
+                </div>`
+    return html;
+  }
+  $('#new_comment').on('submit', function(e){
+    e.preventDefault();
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      $('.comments').append(html);
+      $('.text-box').val('');
+      $('.send-box').prop('disabled', false);
+    })
+  })
 });
